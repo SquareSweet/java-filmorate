0a96e6636098e4fddd5ecf825d55390b85c89ab1
Svetlana Antoshkina <svetlana120595@gmail.com>
2022-08-13 13:24:54 +0300

    test: тесты валидации для UserController

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,274 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    private UserController userController;
+
+    @BeforeEach
+    void updateController() {
+        userController = new UserController();
+    }
+
+    @Test
+    void createValid() {
+        User expectedUser = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        User actualUser = userController.create(expectedUser);
+
+        assertEquals(expectedUser, actualUser, "Пользователи не совпадают");
+    }
+
+    @Test
+    void createInvalidEmail() {
+        User expectedUser = new User(
+                0,
+                "testemail.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.create(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void createEmptyEmail() {
+        User expectedUser = new User(
+                0,
+                "",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.create(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void createInvalidLogin() {
+        User expectedUser = new User(
+                0,
+                "test@email.com",
+                "test login",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.create(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void createEmptyLogin() {
+        User expectedUser = new User(
+                0,
+                "test@email.com",
+                "",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.create(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void createInvalidDate() {
+        User expectedUser = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(2990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.create(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void updateValid() {
+        User user = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+        userController.create(user);
+
+        User expectedUser = new User(
+                0,
+                "test@email.ru",
+                "testLoginUpdated",
+                "Test user updated name",
+                LocalDate.of(1990, 12, 10)
+        );
+
+        User actualUser = userController.update(expectedUser);
+
+        assertEquals(expectedUser, actualUser, "Пользователи не совпадают");
+    }
+
+    @Test
+    void updateInvalidEmail() {
+        User user = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+        userController.create(user);
+
+        User expectedUser = new User(
+                0,
+                "testemail.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.update(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void updateEmptyEmail() {
+        User user = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+        userController.create(user);
+
+        User expectedUser = new User(
+                0,
+                "",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.update(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void updateInvalidLogin() {
+        User user = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+        userController.create(user);
+
+        User expectedUser = new User(
+                0,
+                "test@email.com",
+                "test login",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.update(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void updateEmptyLogin() {
+        User user = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+        userController.create(user);
+
+        User expectedUser = new User(
+                0,
+                "test@email.com",
+                "",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.update(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+
+    @Test
+    void updateInvalidDate() {
+        User user = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(1990, 12, 12)
+        );
+        userController.create(user);
+
+        User expectedUser = new User(
+                0,
+                "test@email.com",
+                "testLogin",
+                "Test user name",
+                LocalDate.of(2990, 12, 12)
+        );
+
+        assertThrows(
+                ValidationException.class,
+                () -> userController.update(expectedUser),
+                "Должно выбрасываться исключение"
+        );
+    }
+}
