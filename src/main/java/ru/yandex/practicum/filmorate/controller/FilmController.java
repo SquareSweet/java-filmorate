92863773318aa66c0a81f26a39dfa40c7c566b43
Svetlana Antoshkina <svetlana120595@gmail.com>
2022-08-13 11:54:49 +0300

    feat: добавить логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+@Slf4j
 @RequestMapping("films")
 public class FilmController {
 
@@ -16,6 +18,7 @@ public class FilmController {
 
     @GetMapping
     List<Film> findAll() {
+        log.info("Получен запрос на полный список фильмов");
         return new ArrayList<>(films.values());
     }
 
@@ -23,6 +26,7 @@ public class FilmController {
     Film create(@RequestBody Film film) {
         if (isValid(film)) {
             films.put(film.getId(), film);
+            log.info("Добавлен фильм id={}", film.getId());
             return film;
         } else {
             throw new ValidationException("Некорректно заполнены поля фильма");
@@ -31,25 +35,33 @@ public class FilmController {
 
     @PutMapping("/{filmId}")
     Film update(@PathVariable Integer filmId, @RequestBody Film film) {
-        if (filmId == film.getId() || !isValid(film)) {
-            films.put(filmId, film);
-            return film;
+        if (filmId != film.getId()) {
+            log.warn("Ошибка при обновлении фильма: не совпадают id");
+            throw new ValidationException("Не совпадают id фильма");
+        } else if (!isValid(film)) {
+            throw new ValidationException("Некорректно заполнены поля пользователя");
         } else {
-            throw new ValidationException("Некорректно заполнены поля фильма");
+            films.put(filmId, film);
+            log.info("Обновлён фильм id={}", filmId);
+            return film;
         }
     }
 
     private boolean isValid(Film film) {
         if (film.getName().isBlank()) {
+            log.warn("Ошибка при добавлении/обновлении фильма id={}: пустое название", film.getId());
             return false;
         }
         if (film.getDescription().length() > 200) {
+            log.warn("Ошибка при добавлении/обновлении фильма id={}: длина описания больше 200 символов", film.getId());
             return false;
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895,12,28))) {
+            log.warn("Ошибка при добавлении/обновлении фильма id={}: некорректная дата релиза", film.getId());
             return false;
         }
         if (film.getDuration().isNegative()) {
+            log.warn("Ошибка при добавлении/обновлении фильма id={}: некорректная продолжительность", film.getId());
             return false;
         }
         return true;
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("users")
 public class UserController {
@@ -17,6 +19,7 @@ public class UserController {
 
     @GetMapping
     List<User> findAll() {
+        log.info("Получен запрос на полный список пользователей");
         return new ArrayList<>(users.values());
     }
 
@@ -24,6 +27,7 @@ public class UserController {
     User create(@RequestBody User user) {
         if (isValid(user)) {
             users.put(user.getId(), user);
+            log.info("Добавлен пользователь id={}", user.getId());
             return user;
         } else {
             throw new ValidationException("Некорректно заполнены поля пользователя");
@@ -32,22 +36,29 @@ public class UserController {
 
     @PutMapping("/{userId}")
     User update(@PathVariable Integer userId, @RequestBody User user) {
-        if (userId == user.getId() || !isValid(user)) {
-            users.put(userId, user);
-            return user;
-        } else {
+        if (userId != user.getId()) {
+            log.warn("Ошибка при обновлении пользователя: не совпадают id");
+            throw new ValidationException("Не совпадают id пользователя");
+        } else if (!isValid(user)) {
             throw new ValidationException("Некорректно заполнены поля пользователя");
+        } else {
+            users.put(userId, user);
+            log.info("Обновлён пользователь id={}", userId);
+            return user;
         }
     }
 
     private boolean isValid(User user) {
         if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.warn("Ошибка при добавлении/обновлении пользователя id={}: некорректный email", user.getId());
             return false;
         }
         if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            log.warn("Ошибка при добавлении/обновлении пользователя id={}: некорректный логин", user.getId());
             return false;
         }
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Ошибка при добавлении/обновлении пользователя id={}: некорректная дата", user.getId());
             return false;
         }
         return true;
